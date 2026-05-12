d31ca59dd338a0e675b0dbbee31e7b7959e1bb94
Frank Purcell <fxpurcell@gmail.com>
2026-05-12 16:45:43 -0700

    fix(config): clean up the PASS / FAIL output

--- ott/gtfs_etl/check.py
+++ ott/gtfs_etl/check.py
@@ -98,14 +98,13 @@ def gtfs_fare_category():
             c = read_zip_file(z, "rider_categories.txt")
             v = str_to_csv(c)
             nc = feed_has_unexpected_categories(z, v, categories)
+            f = os.path.basename(z)
             if nc:
-                print(f" **FAIL**: {z}", end="")
-                f = os.path.basename(z)
                 u = next((i.get('url') for i in feeds if i.get("name") == f), None)
-                print(f" ({u}) has UNKNOWN rider category(s): '{nc}'")
+                print(f" **FAIL**: {f} ({u}) has UNKNOWN rider category(s): '{nc}'")
                 ret_val += 1
             else:
-                print(f" PASS: {z}")
+                print(f" PASS: {f}")
         print("done")
 
     return ret_val
